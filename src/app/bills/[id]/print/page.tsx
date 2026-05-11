c1ea36164a7877035097d79d7681e897b6e47825
SandeepMS07 <sandeepms746@gmail.com>
2026-05-11 19:45:25 +0530

    feat(bill-print): add contact information for Harish Putta and Jagadish in bill print layout

--- src/app/bills/[id]/print/page.tsx
+++ src/app/bills/[id]/print/page.tsx
@@ -134,6 +134,14 @@ export default async function BillPrintPage({
             <div className="bill-print-info-left">
               <div className="bill-print-yard">APMC Yard</div>
               <div>Honnali</div>
+              <div className="bill-print-info-phone">
+                <span className="bill-print-icon"></span>
+                <span>Harish Putta</span> <strong>: 9019800731</strong>
+              </div>
+              <div className="bill-print-info-phone">
+                <span className="bill-print-icon"></span>
+                <span>Jagadish</span> <strong>: 7795953398</strong>
+              </div>
 
               <div className="bill-print-info-phone mt-2">
                 <span>Bags</span>{" "}
